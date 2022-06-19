4cc9279d03a7fc57d59fb1ada44690eb391a806a
Mark Viti <+>
2022-06-19 14:36:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        "My name is \_\_"
+    end 
+
+    get '/hometown' do 
+        "My hometown is \_\_"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is \_\_"
+    end 
+
 end
